d356d653588ee1fb64beef8e56afc88bfe8cc043
Yoshino-s <yoshino.prog@gmail.com>
2020-11-16 11:10:14 +0800

    Fix schedule

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function main() {
   let template = fs.readFileSync(join(__dirname, "template.md")).toString();
   template = template.replace("{{language}}", await generateImage());
   template = template.replace("{{writeup_list}}", await generateWriteup());
-  fs.writeFileSync("dist/README.MD", template);
+  fs.writeFileSync("README.MD", template);
 }
 
 main();
